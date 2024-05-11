cda7cfcec2f5ceb38ccb052077a59a0c37dcb5aa
Rishabh Bambani <rbambani@deloitte.com>
2024-05-11 17:07:56 +0530

    code push for cleaning question

--- server/api/api_logic/question_scrap_helper.py
+++ server/api/api_logic/question_scrap_helper.py
@@ -5,6 +5,14 @@ from bs4 import BeautifulSoup
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+def clean_question(question):
+    question_index_start = question.find("standard output")
+    question_index_later = question.find("Input")
+    question_stripped = question[question_index_start+len("standard output"):question_index_later].strip()
+    index = question.find("time limit")
+    return index, question_stripped
+
+
 def question_scrap_helper(url):
     response = {}
     try:
@@ -14,9 +22,9 @@ def question_scrap_helper(url):
         soup= BeautifulSoup(page_response.text,"html.parser")
         targetted_div = soup.find("div",class_="problem-statement")
         question = targetted_div.getText()
-        index = question.find("time limit")
+        index, question_stripped= clean_question(question)
         response["title"] = question[:index].strip()
-        response["question"] = question
+        response["question"] = question_stripped
         return response
         
         
