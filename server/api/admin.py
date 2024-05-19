455103e7b0987243c42b8bb112dc000187b7204b
Rishabh Bambani <rbambani@deloitte.com>
2024-05-19 19:31:36 +0530

    code push for saving questions in Database

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from api.models import Questions
 # Register your models here.
+
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = ['question_title','question_description']
+
+
+admin.site.register(Questions,QuestionsAdmin)
--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Questions(models.Model):
+    question_title = models.TextField(max_length=50)
+    question_description = models.TextField(max_length=500)
+
+    
